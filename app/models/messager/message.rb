39cc649aea07bc28c061b43113707c6e7698a136
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2016-10-21 13:30:08 -0400

    feat: Add messaging logic to `Message` model
    Add actual messaging logic to the `Message` model.

--- app/models/messager/message.rb
+++ app/models/messager/message.rb
@@ -2,17 +2,23 @@ module Messager
   class Message < ApplicationRecord
     self.table_name = "messages"
 
-    belongs_to :sender, polymorphic: true
+    belongs_to :sender,
+      polymorphic: true
 
-    belongs_to :receiver, polymorphic: true
+    belongs_to :receiver,
+      polymorphic: true
 
-    validates :sender, presence: true
+    validates :sender,
+      presence: true
 
-    validates :receiver, presence: true
+    validates :receiver,
+      presence: true
 
-    validates :subject, presence: true
+    validates :subject,
+      presence: true
 
-    validates :body, presence: true
+    validates :body,
+      presence: true
 
   end
 end
